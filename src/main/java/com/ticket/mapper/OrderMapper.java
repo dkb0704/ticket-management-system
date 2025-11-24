b329b2fc28dab60d321334fbd649bbe1a022d2f4
dkb <2574078960@qq.com>
2025-11-24 22:14:54 +0800

    feat(*):增加了管理员 演出管理的方法 feat(*):增加了管理员 订单管理的方法

--- src/main/java/com/ticket/mapper/OrderMapper.java
+++ src/main/java/com/ticket/mapper/OrderMapper.java
@@ -3,6 +3,8 @@ package com.ticket.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ticket.model.dto.response.AdminOrderDetailResponseDTO;
+import com.ticket.model.dto.response.AdminOrderListResponseDTO;
 import com.ticket.model.dto.response.OrderDetailResponseDTO;
 import com.ticket.model.dto.response.OrderListResponseDTO;
 import com.ticket.model.entity.Order;
@@ -19,6 +21,21 @@ public interface OrderMapper extends BaseMapper<Order> {
 
     // 查询订单详情（关联演出/场次表）
     OrderDetailResponseDTO selectOrderDetail(
+            @Param("id") Long id
+    );
+
+    // 管理员端分页查询订单列表（关联演出/场次/用户表）
+    IPage<AdminOrderListResponseDTO> selectAdminOrderPage(
+            Page<AdminOrderListResponseDTO> page,
+            @Param("orderNo") String orderNo,
+            @Param("userId") Long userId,
+            @Param("performanceId") Long performanceId,
+            @Param("status") Integer status,
+            @Param("keyword") String keyword
+    );
+
+    // 管理员端查询订单详情（关联演出/场次/用户表）
+    AdminOrderDetailResponseDTO selectAdminOrderDetail(
             @Param("id") Long id
     );
 }
